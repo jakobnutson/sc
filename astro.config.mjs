694b1d207d50abb4efe8f8cb853f51d123430394
jakobnutson <51325437+jakobnutson@users.noreply.github.com>
2026-01-05 20:10:09 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jakobnutson.github.io/sc/',
+    site: 'https://jakobnutson.github.io',
   base: '/sc',
   compressHTML: true,
   build: {
